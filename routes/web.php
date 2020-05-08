30e3a9e1d4b1e1458896c9bd2f936eb2a24e48da
lamhung <lamhungypl@gmail.com>
2020-05-09 00:49:56 +0700

    migrating admin login

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin', function () {
-    return view('admin.admin_login');
+Route::match(['get', 'post'], '/admin', function () {
+    return view('AdminController@login');
 });
 
 Auth::routes();
