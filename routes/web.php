b9219f42c3b4db6fd44a3fa72fdf1f11ebb65982
lamhung <lamhungypl@gmail.com>
2020-05-09 00:22:02 +0700

    login and register process

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin.admin_login');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
