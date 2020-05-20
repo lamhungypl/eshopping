bc69e6c524420c50bef1e4ffa7134730827e7802
lamhung <lamhungypl@gmail.com>
2020-05-21 02:00:17 +0700

    setup index html css

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index');
 Route::match(['get', 'post'], '/admin', 'AdminController@login');
 
 Auth::routes();
